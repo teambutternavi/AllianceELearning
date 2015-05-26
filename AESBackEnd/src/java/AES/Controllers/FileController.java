e2a1bdabc7f7fa14f30efe41eb0dac7f4c686619
asi <asi@tbi08>
2015-05-26 12:27:56 +0800

    Added Libraries for Upload Feature

--- AESBackEnd/src/java/AES/Controllers/FileController.java
+++ AESBackEnd/src/java/AES/Controllers/FileController.java
@@ -5,10 +5,25 @@
  */
 package AES.Controllers;
 
+import java.util.Enumeration;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author asi
  */
+@RestController
+@RequestMapping(value = "/file", method = RequestMethod.POST)
 public class FileController {
     
+    @RequestMapping(value = "/upload", method=RequestMethod.GET)
+    public void upload(HttpServletRequest req,@RequestParam Map<String,String> requestParams){
+        
+       
+    }
 }
