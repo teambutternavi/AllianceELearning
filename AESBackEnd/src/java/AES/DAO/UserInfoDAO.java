8a649003fd8ff8392aff48c30a1ff1fc66046b3e
asi <asi@localhost>
2015-05-28 10:43:44 +0800

    Exception handling improvised

--- AESBackEnd/src/java/AES/DAO/UserInfoDAO.java
+++ AESBackEnd/src/java/AES/DAO/UserInfoDAO.java
@@ -32,7 +32,7 @@ public class UserInfoDAO {
         ps.setInt(6,position);
         ps.execute();
         }
-        else throw new Exception("no such user exists, idiot!");
+        else throw new SQLException("user does not exist");
     }
     
     public static void removeUserInfo(int userid)throws IOException, ClassNotFoundException, SQLException
@@ -44,7 +44,7 @@ public class UserInfoDAO {
         ps.execute();}
         else throw new SQLException("user does not exist");
     }
-    public static void editUserInfo(int userid, String firstname,String lastname,int age, String department,int position)throws IOException, ClassNotFoundException, SQLException, Exception
+    public static void editUserInfo(int userid, String firstname,String lastname,int age, String department,int position)throws IOException, ClassNotFoundException, SQLException
     { if(contains(userid)){
         String query="Update userinfo set firstname=?,lastname=?,age=?,department=?,position=? where userid=?";
         PreparedStatement ps=DatabaseManager.getInstance().getStatement(query);
@@ -56,7 +56,7 @@ public class UserInfoDAO {
         ps.setInt(6,userid);
         ps.execute();
         }
-        else throw new Exception("user does not exist");
+        else throw new SQLException("user does not exist");
     }
     public static boolean contains(int id)throws IOException, ClassNotFoundException, SQLException
     {
@@ -66,7 +66,7 @@ public class UserInfoDAO {
         ResultSet rs=ps.executeQuery();
         return rs.next();
     }
-    public static Userinfo getUserInfoById(int id)throws IOException, ClassNotFoundException, SQLException, Exception
+    public static Userinfo getUserInfoById(int id)throws IOException, ClassNotFoundException, SQLException
     {
         if(contains(id)){
         Userinfo user=new Userinfo();
@@ -84,9 +84,9 @@ public class UserInfoDAO {
         user.setUser(UserDAO.getUserByID(rs.getInt("userid")));
         user.getUser().setUserinfo(user);
         return user;}
-        else throw new Exception("user does not exist");
+        else throw new SQLException("user does not exist");
     }
-    public static List<Userinfo> getAllUserInfo()throws IOException, ClassNotFoundException, SQLException, Exception
+    public static List<Userinfo> getAllUserInfo()throws IOException, ClassNotFoundException, SQLException
     {
         List<Userinfo> user=new ArrayList();
         String query="Select * from userinfo";
@@ -105,7 +105,7 @@ public class UserInfoDAO {
         }
         return user;
     }
-    public static List<Userinfo> getAllUserByDepartment(String department)throws IOException, ClassNotFoundException, SQLException, Exception
+    public static List<Userinfo> getAllUserByDepartment(String department)throws IOException, ClassNotFoundException, SQLException
   {
      List<Userinfo> user=new ArrayList();
         String query="Select * from userinfo where department=?";
@@ -127,7 +127,7 @@ public class UserInfoDAO {
         return user;
   }
     
-     public static List<Userinfo> getAllUserByPosition(int pos)throws IOException, ClassNotFoundException, SQLException, Exception
+     public static List<Userinfo> getAllUserByPosition(int pos)throws IOException, ClassNotFoundException, SQLException
   {
       if(pos<4&&pos>-1){
      List<Userinfo> user=new ArrayList();
@@ -148,12 +148,12 @@ public class UserInfoDAO {
         user.add(use);
         }
         return user;}
-      else throw new Exception("position not valid");
+      else throw new SQLException("position not valid");
   }
  
     public static void main(String args[])throws IOException, ClassNotFoundException, SQLException, Exception
     {
-
+        
        
     }
 }
